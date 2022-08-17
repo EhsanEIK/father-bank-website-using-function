5c84c5433630c0a0a635d3fa2594c9450083f42f
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-08-17 11:10:41 +0600

    added validation in deposit and withdraw input field

--- js/deposit.js
+++ js/deposit.js
@@ -2,6 +2,10 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     const depositAmount = getElementValueById('deposit-field');
     const previousDepositBalance = getInnerTextById('deposit-balance');
     const totalBalance = getInnerTextById('total-balance');
+    if (isNaN(depositAmount)) {
+        alert("Please Provide A Number");
+        return;
+    }
 
     const newDepositBalance = add(previousDepositBalance, depositAmount);
     setInnerTextById('deposit-balance', newDepositBalance);
--- js/withdraw.js
+++ js/withdraw.js
@@ -2,6 +2,10 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const withdrawAmount = getElementValueById('withdraw-field');
     const previousWithdarwBalance = getInnerTextById('withdraw-balance');
     const totalBalance = getInnerTextById('total-balance');
+    if (isNaN(withdrawAmount) || withdrawAmount > totalBalance) {
+        alert("Please Provide Amount in Number within Balance");
+        return;
+    }
 
     const newWithdrawBalance = add(previousWithdarwBalance, withdrawAmount);
     setInnerTextById('withdraw-balance', newWithdrawBalance);
